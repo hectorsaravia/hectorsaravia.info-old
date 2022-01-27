c6c87949fb42f985dbcaa2929dbcf1c4f657e8bf
hectorsaravia <hector.saravia@mail.udp.cl>
2022-01-26 23:57:59 -0300

    -Imported aws credentials -Checks if the credentials are valid -Added comments

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from aws_credetianls import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY
 
+#Defines te use of a flask app
 app = Flask(__name__)
 
+#Index flask route
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    if (AWS_SECRET_ACCESS_KEY != None and AWS_ACCESS_KEY != None):
+        return ("Everything seems to be working for now...")
+    else:
+        return ("Something went wrong with your AWS credentials")
 
+#run settings
 app.run(host = '0.0.0.0', port = 80)
