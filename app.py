80b8370718dc4e0b2979ae3728720783491979d7
hectorsaravia <hector.saravia@mail.udp.cl>
2022-01-27 00:57:55 -0300

    Added s3 resource and hability download a file

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
-from aws_credetianls import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY
+from aws_credetianls import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY, BUCKET_NAME, OBJECT_NAME, FILE_NAME
+import boto3
+
+#Declaration of AWS credentials to use the s3 resource
+s3 = boto3.client('s3',
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name='sa-east-1'
+)
 
 #Defines te use of a flask app
 app = Flask(__name__)
@@ -8,6 +16,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     if (AWS_SECRET_ACCESS_KEY != None and AWS_ACCESS_KEY != None):
+        s3.Bucket('BUCKET_NAME').download_file('OBJECT_NAME', 'FILE_NAME')
         return ("Everything seems to be working for now...")
     else:
         return ("Something went wrong with your AWS credentials")
--- aws_credetianls.py
+++ aws_credetianls.py
@@ -2,3 +2,6 @@ import os
 
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+OBJECT_NAME = os.environ.get('OBJECT_NAME')
+FILE_NAME = os.environ.get('FILE_NAME')
