b54a00a9fe245d902cb24f1bc63d44d8ba9fc4e9
hectorsaravia <hector.saravia@mail.udp.cl>
2022-07-25 09:07:15 -0400

    Fixed CV2.pdf reference

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 #Index flask route
 @app.route("/")
 def hello():
-    if ( not os.path.exists('/CV.pdf') ):
+    if ( not os.path.exists('/CV2.pdf') ):
         s3.download_file(BUCKET_NAME,OBJECT_NAME,'/CV2.pdf')
     return send_from_directory('/', 'CV2.pdf')
 
